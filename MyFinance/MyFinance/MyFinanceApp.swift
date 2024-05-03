50aee3c193ceb60b87197461cdade16a6f0b69ae
Sahil Rai <sahil.rai@ballys.com>
2024-05-03 17:33:35 +0100

    first commit

---
+++ MyFinance/MyFinance/MyFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceApp.swift
+//  MyFinance
+//
+//  Created by Sahil Rai on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
